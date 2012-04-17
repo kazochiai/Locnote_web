b008676584f1fbbf4762d10a0b5e9a1b692e39c8
Kazuhito Ochiai <kazu_ochiai@me.com>
2012-04-17 00:20:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locnote::Application.config.secret_token = 'd7330c202554f9c49e8e504b8cee5c717ec68fcafd03d5986ee35fe8873d174242ec0cb84a6a07ad1e8000ca68eff8d7c83c40b2da1d93a82cea5914efb92242'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Locnote::Application.config.session_store :cookie_store, :key => '_locnote_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Locnote::Application.config.session_store :active_record_store
